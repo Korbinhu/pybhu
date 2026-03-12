3d6fc60c1087022cec37d84840fc52c2f9b99f25
Korbin <37903338+Korbinhu@users.noreply.github.com>
2026-03-12 18:41:36 +0000

    Delete src/stack_viewer directory

--- src/stack_viewer/__init__.py
+++
@@ -1,4 +0,0 @@
-from .api import view_stack
-from .viewer import ImageStackViewer
-
-__all__ = ["ImageStackViewer", "view_stack"]
--- src/stack_viewer/api.py
+++
@@ -1,7 +0,0 @@
-from .viewer import ImageStackViewer
-
-
-def view_stack(data, **options):
-    viewer = ImageStackViewer(data, **options)
-    viewer.show()
-    return viewer
