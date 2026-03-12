e148c0192c7e0e77f49a152a7871fce3b81de63a
Korbin <37903338+Korbinhu@users.noreply.github.com>
2026-03-12 19:51:07 +0000

    Delete src directory

--- src/img_viewer/__init__.py
+++
@@ -1,4 +0,0 @@
-from .api import img_viewer
-from .viewer import ImageStackViewer
-
-__all__ = ["ImageStackViewer", "img_viewer"]
--- src/img_viewer/api.py
+++
@@ -1,7 +0,0 @@
-from .viewer import ImageStackViewer
-
-
-def img_viewer(data, **options):
-    viewer = ImageStackViewer(data, **options)
-    viewer.show()
-    return viewer
